c37bc5701f32aa25bb387876b5bf1077b251cb9f
Jackoday <>
2021-05-06 06:53:08 +0000

    Finished for submission.

--- main.py
+++ main.py
@@ -97,43 +97,62 @@ def combPermRec(n,r):
     else:
       return n * fact(n-1)
 
+  starttime = time.time()
   def comb(n,r):
     x = fact(r+n-1)
     y = fact(r)
     z = fact(n-1)
     return (x/(y*z))
   print ("Combination of (" + str(n) + ", " + str(r) + ") = " + str(int(comb(n,r))))
+  print ("Time: " + str((time.time()-starttime)*1000))
 
+  starttime = time.time()
   def perm(n,r):
     if (r != 0):
       return (n * perm(n, r - 1))
     else:
         return 1;
   print ("Permutation of (" + str(n) + ", " + str(r) + ") = " + str(perm(n,r)))
+  print ("Time: " + str((time.time()-starttime)*1000) + "\n") 
 
-starttime = time.time()
 combPermRec(4,2)
-print ("Time: " + str((time.time()-starttime)*1000) + "\n")
 
 #Combinations and permutations iterative
 print ("Combinations and permutations - Iterative\n----------")
 def combPermIt(n,r):
 
-  #def comb(n,r)
+  starttime = time.time()
+  def comb(n,r):
 
+    a = (n+r-1)
+    for i in range(a-1):
+      a = a*((n+r-1)-(i+1))
+
+    b = r
+    for i in range(b-1):
+      b = b*(r-(i+1))
+
+    c = (n-1)
+    for i in range(c-1):
+      c = c*((n-1)-(i+1))
+
+    return (a/(b*c))
+  print ("Combinstion of (" + str(n) + ", " + str(r) + ") = " + str(int(comb(n,r))))
+  print ("Time: " + str((time.time()-starttime)*1000))
+
+  starttime = time.time()
   def perm(n,r):
     pow = 1
     for i in range (r):
       pow = pow * n
     return pow
   print ("Permutation of (" + str(n) + ", " + str(r) + ") = " + str(perm(n,r)))
+  print ("Time: " + str((time.time()-starttime)*1000) + "\n")
 
-starttime = time.time()
 combPermIt(4,2)
-print ("Time: " + str((time.time()-starttime)*1000) + "\n")
 
 
-print ("\nBIT605 Assignment 2 part 2\n----------\n")
+print ("\nBIT605 Assignment 2 part 2\n----------")
 
 class user:
   def __init__(self, ID, fName, lName, email):
@@ -168,105 +187,10 @@ with open("BIT605_unsorted_data.csv") as csv_file:
 bubbleList = list_unsorted
 starttime = time.time()
 bubbleSort(bubbleList)
-print ("Time to sort data with bubble sort: " + str((time.time()-starttime)*1000) + "\n")
+print ("Time to sort data with bubble sort: " + str((time.time()-starttime)*1000))
 
 #Run selection sort
 selectionList = list_unsorted
 starttime = time.time()
 selectionSort(selectionList)
-print ("Time to sort data with selection sort: " + str((time.time()-starttime)*1000) + "\n")
-
-#Binary Search Tree
-class BSTNode:
-  def __init__(self, val=None):
-    self.left = None
-    self.right = None
-    self.val = val
-
-  def insert(self, val):
-    if not self.val:
-      self.val = val
-      return
-
-    if self.val == val:
-      return
-
-    if val < self.val:
-      if self.left:
-        self.left.insert(val)
-        return
-      self.left = BSTNode(val)
-      return
-
-    if self.right:
-      self.right.insert(val)
-      return
-    self.right = BSTNode(val)
-
-  def get_min(self):
-    current = self
-    while current.left is not None:
-      current = current.left
-    return current.val
-
-  def get_max(self):
-    current = self
-    while current.right is not None:
-      current = current.right
-    return current.val
-
-  def preorder(self, vals):
-    if self.val is not None:
-      vals.append(self.val)
-    if self.left is not None:
-      self.left.preorder(vals)
-    if self.right is not None:
-      self.right.preorder(vals)
-    return vals
-
-  def inorder(self, vals):
-    if self.left is not None:
-      self.left.inorder(vals)
-    if self.val is not None:
-      vals.append(self.val)
-    if self.right is not None:
-      self.right.inorder(vals)
-    return vals
-
-  def exists(self, val):
-    if val == self.val:
-      return True
-
-    if val < self.val:
-      if self.left == None:
-        return False
-      return self.left.exists(val)
-
-    if self.right == None:
-      return False
-    return self.right.exists(val)
-
-def height(root):
-  #if root is None return 0
-    if root==None:
-      return 0
-    #find height of left subtree
-    hleft=height(root.leftChild)
-    #find the height of right subtree
-    hright=height(root.rightChild)  
-    #find max of hleft and hright, add 1 to it and return the value
-    if hleft>hright:
-      return hleft+1
-    else:
-      return hright+1
-
-bst = BSTNode()
-for user in list_unsorted:
-  bst.insert(user.ID)
-
-print("Number exists:")
-print(bst.exists(647))
-
-print(height())
-
-print(bst.inorder([]))
+print ("Time to sort data with selection sort: " + str((time.time()-starttime)*1000))
